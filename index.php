a6889922f9e394cb0b6dd3a36dd791bac323c558
vytautasjaz <vytautasjaz@gamil.com>
2019-10-29 15:52:33 +0200

    Uzduotis 2

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <html>
   <head>
     <title>Aš valdau HTML <?php print 'ir PHP'; ?>!</title>
-    <style>
-        .melyna {
-            color:blue;
-        }
-    </style>
-  </head>
+    </head>
   <body>
       <h1><b>Vytautas</b> - HTML <?php print 'ir PHP'; ?>asas!</h1> 
     <p>Moku kurti dinaminį turinį. Atidaryta <?php print date("Y-m-d") ?>, <?php print date("h:i:sa")?></p>
