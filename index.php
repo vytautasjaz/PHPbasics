dd906f0d3903a4661a9aac99a42455588059faa8
vytautasjaz <vytautasjaz@gamil.com>
2019-10-29 16:45:34 +0200

    Stringai

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
-  <head>
-    <title>Aš, <?php print date('l') ?> ir PHP</title>
-  </head>
-  <body>
-      <h1><b>Vytautas</b> - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('o').' metų' ?></h1> 
+    <head>
+        <title><?php print 'Aš, ' . date('l') . ' ir PHP'; ?></title>
+    </head>
+    <body>
+        <h1><b>Vytautas</b> - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('o') . ' metų'; ?></h1> 
         <p> 
-            Viskas prasidėjo <?php print date('F').' mėnesio '.date ('j').' dieną!'?>
+            Viskas prasidėjo <?php print date('F') . ' mėnesio ' . date('j') . ' dieną!'; ?>
         </p>       
-  </body>
+    </body>
 </html>
