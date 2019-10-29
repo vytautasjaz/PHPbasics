1c328f2fc95317eae7f8d569b5a008a2c87e28a8
vytautasjaz <vytautasjaz@gamil.com>
2019-10-29 17:56:32 +0200

    Stringai random

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
     <head>
-        <title>PHP lydės ir <?php print date ('Y-m-d', strtotime('+' . rand(0,10) . 'years')) ;?></title>
+        <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+' . rand(0, 10) . 'years')); ?></title>
     </head>
     <body>
-        <h1><b>Vytautas</b> - Galbūt turėsiu <?php print rand(1,5) . ' vaikus' ?></h1>
-                <p>D. Trump'as nebus prezidentu <?php print date ('Y', strtotime('+' . rand(2,10) . 'years')). date (', m, d') ?></h1>
-    </body>
+        <h1><b>Vytautas</b> - Galbūt turėsiu <?php print rand(1, 5) . ' vaikus' ?></h1>
+        <p>D. Trump'as nebus prezidentu <?php print date('Y', strtotime('+' . rand(2, 10) . 'years')) . date('m') . ' mėnuo, diena ' . date('d'); ?></h1>
+</body>
 </html>
