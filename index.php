50fb214df3cba91dc90fc1c985fc885cfbe71928
vytautasjaz <vytautasjaz@gamil.com>
2019-10-29 15:08:41 +0200

    Uzduotis 1

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php
-print 'Veikia!';
+<!DOCTYPE html>
+<html>
+  <head>
+gi    <title>Aš valdau HTML!</title>
+  </head>
+  <body>
+    <h1>Vytautas - HTML asas!</h1>
+    <p>Moku kurti statinį turinį</p>
+  </body>
+</html>
