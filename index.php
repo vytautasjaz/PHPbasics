9a07b34d7c40f00b98759a510ceacd69cf227042
vytautasjaz <vytautasjaz@gamil.com>
2019-10-29 16:00:25 +0200

    Uzduotis 2

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
   <head>
     <title>Aš valdau HTML <?php print 'ir PHP'; ?>!</title>
-    </head>
+  </head>
   <body>
       <h1><b>Vytautas</b> - HTML <?php print 'ir PHP'; ?>asas!</h1> 
-    <p>Moku kurti dinaminį turinį. Atidaryta <?php print date("Y-m-d") ?>, <?php print date("h:i:sa")?></p>
+    <p>Moku kurti dinaminį turinį. Atidaryta: <?php print date("Y-m-d, h:i a") ?></p>
   </body>
 </html>
