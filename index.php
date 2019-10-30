048c1a96d4cb00c8b017a8251dc0a5ed16793349
vytautasjaz <vytautasjaz@gamil.com>
2019-10-30 15:12:46 +0200

    Random HEADE

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <html>
     <head>
         <title>Užduotis 8</title>
+        <style>
+            body {
+                background-color:rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255); ?>);
+            } 
+
+            h1 {
+                font-size:<?php print rand(10, 50); ?>px;
+            }
+
+            p {
+                color:rgb(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>);
+            }
+        </style>
     </head>
-    <body style="background-color:rgb(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>);">
-        <h1 style="font-size:<?php print rand(10, 30) ?> "> Aš keičiu savo dydį!</h1>
-        <p style="color:rgb(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>);"> Aš keičiu savo spalvą!</p>
+    <body>
+        <h1> Aš keičiu savo dydį!</h1>
+        <p> Aš keičiu savo spalvą!</p>
     </body>
 </html>
