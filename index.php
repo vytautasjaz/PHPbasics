18a0b454ce8d9b5d4ec78d26af0b2f01d5730826
vytautasjaz <vytautasjaz@gamil.com>
2019-10-30 15:24:37 +0200

    DICE

--- index.php
+++ index.php
@@ -1,22 +1,28 @@
 <html>
     <head>
-        <title>Užduotis 8</title>
+        <title>CSS ir PHP</title>
         <style>
-            body {
-                background-color:rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255); ?>);
-            } 
-
-            h1 {
-                font-size:<?php print rand(10, 50); ?>px;
+            .dice1 {
+                background-image: url("http://dobbelsteen.virtuworld.net/img/1c.gif");
             }
-
-            p {
-                color:rgb(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>);
+            .dice2 {
+                background-image: url("http://dobbelsteen.virtuworld.net/img/2c.gif");
+            }
+            .dice3 {
+                background-image: url("http://dobbelsteen.virtuworld.net/img/3c.gif");
+            }
+            .dice4 {
+                background-image: url("http://dobbelsteen.virtuworld.net/img/4c.gif");
+            }
+            .dice5 {
+                background-image: url("http://dobbelsteen.virtuworld.net/img/5c.gif");
+            }
+            .dice6 {
+                background-image: url("http://dobbelsteen.virtuworld.net/img/6c.gif");
             }
         </style>
     </head>
     <body>
-        <h1> Aš keičiu savo dydį!</h1>
-        <p> Aš keičiu savo spalvą!</p>
+        <div class="dice<?php print rand(1, 6); ?>"  style="width: 250px; height: 250px;">
     </body>
 </html>
