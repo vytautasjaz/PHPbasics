783083be49e4b26fcbf58cfc71386e97b6eab300
vytautasjaz <vytautasjaz@gamil.com>
2019-10-31 15:27:36 +0200

    Kintamieji

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-$string = 'Ačiū už dėmesį';
-$int = 45646;
-$bool = true;
-$null = null;
+$img_1 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b5133b75394159.5d83874382b87.png';
+$img_2 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/98afcf75394159.5c4b246bb3e32.png';
+$img_3 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/d9260b75394159.5c4b246bb3734.png';
+$img_4 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/67b01975394159.5c4b246bb3360.png';
 ?>
+<!DOCTYPE html>
 <html>
     <head>
-        <title>Kintamieji</title>
+        <title>Paveikslėliai</title>
     </head>
     <body>
-        <ul>
-            <li>String Tipas: <?php print $string;?></li>
-            <li>Integer Tipas: <?php print $int;?></li>
-            <li>Boolean Tipas: <?php print $bool;?></li>
-            <li>Null Tipas: <?php print $null;?></li>
-        </ul>
+        <img src="<?php print $img_1 ?>">
+        <img src="<?php print $img_2 ?>">
+        <img src="<?php print $img_3 ?>">
+        <img src="<?php print $img_4 ?>">
     </body>
 </html>
