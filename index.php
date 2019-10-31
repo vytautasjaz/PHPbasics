d0e0f9ca3640fcf6ce65c6230246dbacb821045c
vytautasjaz <vytautasjaz@gamil.com>
2019-10-31 15:33:17 +0200

    Kintamieji

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-$img_1 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b5133b75394159.5d83874382b87.png';
-$img_2 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/98afcf75394159.5c4b246bb3e32.png';
-$img_3 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/d9260b75394159.5c4b246bb3734.png';
-$img_4 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/67b01975394159.5c4b246bb3360.png';
+$img = 'src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/b5133b75394159.5d83874382b87.png"';
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,9 +7,9 @@ $img_4 = 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/67b019753
         <title>Paveikslėliai</title>
     </head>
     <body>
-        <img src="<?php print $img_1 ?>">
-        <img src="<?php print $img_2 ?>">
-        <img src="<?php print $img_3 ?>">
-        <img src="<?php print $img_4 ?>">
+        <img <?php print $img; ?>>
+        <img <?php print $img; ?>>
+        <img <?php print $img; ?>>
+        <img <?php print $img; ?>>
     </body>
 </html>
