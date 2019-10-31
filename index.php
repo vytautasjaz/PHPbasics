e52781f61343973908336b0cca3a830a2c4552db
vytautasjaz <vytautasjaz@gamil.com>
2019-10-31 17:04:29 +0200

    Kelione

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <?php
-$bin_vol = 40;
-$bin_heap_vol = rand(1, 15);
-$trash_per_day = 15;
-$days = ($bin_vol + $bin_heap_vol) / $trash_per_day;
-$days_round = round($days, 0, PHP_ROUND_HALF_DOWN);
+$distance = rand(1, 500);
+$consumption = 7.5;
+$price_1 = 1.3;
+
+$fuel_total = round((($distance / 100) * $consumption), 2);
+$price_trip = round(($fuel_total * $price_1), 2);
+
+$h_1 = "Kelionės skaičiuoklė";
+$li_1 = "Nuvažiuota distancija: $distance";
+$li_2 = "Sunaudota $fuel_total l kuro.";
+$li_3 = "Kaina: $price_trip pinigų.";
+$title = $h_1;
 ?>
 <!DOCTYPE htm>
 <html>
     <head>
-        <title>Šiukšlinė</title>
+        <title><?php print $title; ?></title>
     </head>
     <body>
-        <h1>Šiukšlių prognozė</h1>
-        <p>Turima šiukšlinė - <?php print $bin_vol; ?> litrų</p>
-        <p>Žmona nieko nesako, kol kaupas neviršija <?php print $bin_heap_vol; ?> litrų</p>
-        <h3>Išvada: Nieko nedarysiu <?php print $days_round; ?> dienas</h3>
+        <h1><?php print $h_1; ?></h1>
+        <ul>
+            <li><?php print $li_1; ?></li>
+            <li><?php print $li_2; ?></li>
+            <li><?php print $li_3; ?></li>
+        </ul>
     </body>
 </html>
