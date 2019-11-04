1d06ef2e52dd44ae2b64906111d498f8aef963b3
vytautasjaz <vytautasjaz@gamil.com>
2019-11-04 17:33:50 +0200

    Laikas

--- index.php
+++ index.php
@@ -1,43 +1,37 @@
 <?php
-$bool = true;
-$int = 1;
-$str = '1';
-$str_1 = "1.23";
-$flt = 1.23;
+$sekundė = date('s');
 
-
-if ($bool === $int) {
-    $atsakymas_1 = 'Identiški';
-} elseif ($bool == $int) {
-    $atsakymas_1 = 'Lygūs';
+if ($sekundė % 2) {
+    $klase = 'kvadratas';
+} else {
+    $klase = 'apskritimas';
 }
-
-if ($str === $bool) {
-    $atsakymas_2 = 'Identiški';
-} elseif ($str == $bool) {
-    $atsakymas_2 = 'Lygūs';
-}
-
-if ($flt === $str_1) {
-    $atsakymas_3 = 'Identiški';
-} elseif ($flt == $str_1) {
-    $atsakymas_3 = 'Lygūs';
-}
-
-$li_1 = "Bool (true) ir integer (1): $atsakymas_1";
-$li_2 = "String (1) ir Boolean (1): $atsakymas_2";
-$li_3 = "Float (1.23) ir String (1.23): $atsakymas_3";
 ?>
 <html>
     <head>
-        <title> Lygūs ar Identiški</title>
+        <style>
+            div {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 300px;
+                width: 300px;
+                border-collapse: collapse;
+                background-color: red;
+            }
+
+            .kvadratas {
+                border-radius: 0px;
+            }
+            .apskritimas{
+                border-radius: 150px;
+            }
+
+        </style>
+        <title> Sekundės</title>
     </head>
     <body>
-        <ul>
-            <li><?php print $li_1; ?></li>
-            <li><?php print $li_2; ?></li>
-            <li><?php print $li_3; ?></li>
-
-        </ul>
+        <div class="<?php print $klase; ?>">
+            <?php print $sekundė; ?></div>
     </body>
 </html>
