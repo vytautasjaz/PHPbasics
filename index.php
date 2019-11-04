6a729449939676d45fb7a07601b7b2cf6480eb32
vytautasjaz <vytautasjaz@gamil.com>
2019-11-04 15:00:26 +0200

    isgeres

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
 <?php
-$distance = rand(1, 5000);
-$consumption = 7.5;
-$price_1 = 1.3;
-$my_money = 100;
+$grizai_velai = rand(0, 1);
+$grizai_isgeres = rand(0, 1);
 
-$fuel_total = round($distance / 100 * $consumption, 2);
-$price_trip = round($fuel_total * $price_1, 2);
-
-if ($my_money >= $price_trip) {
-    $kelione = "įperkama";
+if ($grizai_velai && !$grizai_isgeres) {
+    $h2 = 'Grįžai vėlai';
+} elseif ($grizai_velai && $grizai_isgeres) {
+    $h2 = 'Grįžai vėlai ir išgėręs';
+} elseif (!$grizai_velai && $grizai_isgeres) {
+    $h2 = 'Grįžai išgėręs';
 } else {
-    $kelione = "neįperkama";
+    $h2 = 'Nieko nepadarei';
 }
 
-$h_1 = "Kelionės skaičiuoklė";
-$li_1 = "Nuvažiuota distancija: $distance";
-$li_2 = "Sunaudota $fuel_total l kuro.";
-$li_3 = "Kaina: $price_trip pinigų.";
-$li_4 = "Turimi pinigai: $my_money";
-$p_1 = "Išvada: Kelionė $kelione";
-$title = $h_1;
+$h1 = 'Buitinė skaičiuoklė';
+$title = $h1;
 ?>
 <!DOCTYPE htm>
 <html>
@@ -27,14 +21,7 @@ $title = $h_1;
         <title><?php print $title; ?></title>
     </head>
     <body>
-        <h1><?php print $h_1; ?></h1>
-        <ul>
-            <li><?php print $li_1; ?></li>
-            <li><?php print $li_2; ?></li>
-            <li><?php print $li_3; ?></li>
-            <li><?php print $li_4; ?></li>
-            <hr>
-            <p><?php print $p_1; ?></p>
-        </ul>
+        <h1><?php print $h1; ?></h1>
+        <h2><?php print $h2; ?></h2>
     </body>
 </html>
