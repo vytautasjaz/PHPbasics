2e6a6324cf799080f5da7fa942a3361473ab3e42
vytautasjaz <vytautasjaz@gamil.com>
2019-11-05 14:52:25 +0200

    Katasuniai

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?php
-$x = rand(0, 15);
-for ($y = 0; $y < $x; $y++) {
-    var_dump("Tai yra $y-tasis ciklas");
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$pavyko = rand(0, 1);
+$katasunis = 0;
+
+for ($x = 0; $x < $kates; $x++) {
+    for ($y = 0; $y < $sunys; $y++) {
+        if ($pavyko) {
+            $katasunis++;
+            break;
+        }
+    }
 }
+
+$h1 = "Katašunių išeiga";
+$h2 = "Dalyvavo $kates katės ir $sunys šunys";
+$h3 = "Katašunių išeiga: $katasunis";
 ?>
 <html>
     <head>
-        <title> Ciklai</title>
+        <title>Ciklai</title>
     </head>
     <body>
+        <h1><?php print $h1 ?></h1>
+        <h2><?php print $h2 ?></h2>
+        <h3><?php print $h3 ?></h3>
     </body>
 </html>
