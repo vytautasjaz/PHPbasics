5f82d6d5e29f48c3be1bef621132a19f1d0d0e52
vytautasjaz <vytautasjaz@gamil.com>
2019-11-05 14:34:15 +0200

    Ciklkai

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-for ($x=rand(0,15); $x>0; $x--){
-    print var_dump("Tai yra $x-tasis ciklas");
+$x = rand(0, 15);
+for ($y = 0; $y < $x; $y++) {
+    var_dump("Tai yra $y-tasis ciklas");
 }
 ?>
 <html>
     <head>
-             <title> Ciklai</title>
+        <title> Ciklai</title>
     </head>
     <body>
-
     </body>
 </html>
