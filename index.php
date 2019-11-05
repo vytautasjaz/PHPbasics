7e725576c2700cf6d52110d26b7e7395436d02b7
vytautasjaz <vytautasjaz@gamil.com>
2019-11-05 16:54:37 +0200

    Ciges

--- index.php
+++ index.php
@@ -1,20 +1,37 @@
 <?php
-$months = 12;
-$wallet = 1000;
-$month_income = 700;
+$days = 365;
+$pack_price = 3.50;
+$count_ttl = 0;
 
-for ($x = 0; $x < $months; $x++) {
-    $month_expenses = rand(0, 1500);
-    $wallet += $month_income - $month_expenses;
-    if ($wallet < 0) {
-        $h2 = "Atsargiai, $x mėnesį gali baigtis pinigai!";
-        break;
-    } else {
-        $h2 = "Po $months mėn., prognozuojamas likutis: $wallet";
+$weeks = round($days / 7, 0, PHP_ROUND_HALF_DOWN);
+$like_dienos = $days % 7;
+
+for ($i = 1; $i <= $weeks; $i++) {
+    for ($x = 1; $x <= 7; $x++) {
+        if ($x <= 5) {
+            $count_ttl += rand(3, 4);
+        } elseif ($x == 6) {
+            $count_ttl += rand(10, 20);
+        } elseif ($x == 7) {
+            $count_ttl += rand(1, 3);
+        }
     }
 }
 
-$h1 = "Wallet forecast";
+$savaites_diena = date('N');
+if ($savaites_diena <= 5) {
+    $count_ttl += rand(3, 4);
+} elseif ($savaites_diena == 6) {
+    $count_ttl += rand(10, 20);
+} elseif ($savaites_diena == 7) {
+    $count_ttl += rand(1, 3);
+}
+
+$pack_count = round($count_ttl / 20, 0, PHP_ROUND_HALF_UP);
+$price_ttl = $pack_count * $pack_price;
+
+$h1 = 'Mano dūmų skaičiuoklė';
+$h2 = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
 ?>
 <html>
     <head>
