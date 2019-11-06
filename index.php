de90a0858b05c363c70f4a4b91a36ab07dce1d71
vytautasjaz <vytautasjaz@gamil.com>
2019-11-06 15:58:41 +0200

    Griaustinis

--- index.php
+++ index.php
@@ -2,19 +2,18 @@
 $speed_of_sound = 333; // m/s
 $max_db = 120; // dB
 $db_decr = 6; // dB
+$atsakymas='';
 
+for ($dist = 1; $dist < 10000; $dist *= 2) {
+    $sec = round($dist / $speed_of_sound);
+    $db = $max_db - ($sec * $db_decr);
+    $atsakymas .= "Po $sec ($dist) m: $db db <br>";
+}
 $h1 = 'Griaustinio zona';
 ?>
 <html>
     <body>
         <h1><?php print $h1; ?></h1>
-        <?php
-        for ($dist = 1; $dist < 10000; $dist *= 2):
-            $sec = round($dist / $speed_of_sound);
-            $db = $max_db - ($sec * $db_decr);
-            $atsakymas="Po $sec ($dist) m: $db";
-            ?>
-            <p><?php print $atsakymas; ?></p>
-        <?php endfor; ?>
-    </body>
+<?php print $atsakymas; ?></p>
+</body>
 </html> 
