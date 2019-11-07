20ef886ce70c8cbc8f6d7452e17a6a34c44fae8a
vytautasjaz <vytautasjaz@gamil.com>
2019-11-07 17:18:34 +0200

    Array led

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
 <?php
 
-//kryptis nusakoma vaziavimo kryptimi pagal laikrodzio rodykle;
-// pusukis i desine - true, i kaire - false;
+//eilutes skaiciuojamos is virsaus i apacia;
+//nesviecia - 0, zalia - 1, geltona - 2, raudona - 3;
 
-$posukiai = [
-    [
-        'kryptis' => true,
-        'laipsnis' => 90
-    ],
-    [
-        'kryptis' => false,
-        'laipsnis' => 160
-    ],
-    [
-        'kryptis' => true,
-        'laipsnis' => 180
-    ],
-    [
-        'kryptis' => false,
-        'laipsnis' => 110
-    ],
-    [
-        'kryptis' => false,
-        'laipsnis' => 180
-    ],
-    ['kryptis' => true,
-        'laipsnis' => 100
-    ]
+$led = [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 3, 3, 3, 3, 2, 1],
+    [1, 2, 3, 2, 2, 3, 2, 1],
+    [1, 2, 3, 2, 2, 3, 2, 1],
+    [1, 2, 3, 3, 3, 3, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
-var_dump($posukiai);
+var_dump($led);
