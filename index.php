3f4a288719382f74c120d562e85426af99cba354
vytautasjaz <vytautasjaz@gamil.com>
2019-11-07 17:02:54 +0200

    Array trasa

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
 <?php
 
-//kryptis nusakoma vaziavimo kryptimi pagal laikrodzio rodykle
+//kryptis nusakoma vaziavimo kryptimi pagal laikrodzio rodykle;
+// pusukis i desine - true, i kaire - false;
 
 $posukiai = [
     [
-        'kryptis' => 'desine',
+        'kryptis' => true,
         'laipsnis' => 90
     ],
     [
-        'kryptis' => 'kaire',
+        'kryptis' => false,
         'laipsnis' => 160
     ],
     [
-        'kryptis' => 'desine',
+        'kryptis' => true,
         'laipsnis' => 180
     ],
     [
-        'kryptis' => 'kaire',
+        'kryptis' => false,
         'laipsnis' => 110
     ],
     [
-        'kryptis' => 'kaire',
+        'kryptis' => false,
         'laipsnis' => 180
     ],
-    ['kryptis' => 'desine',
+    ['kryptis' => true,
         'laipsnis' => 100
     ]
 ];
