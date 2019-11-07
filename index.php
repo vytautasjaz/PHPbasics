396364af71e64eb0c6d808871be667f67a0c897e
vytautasjaz <vytautasjaz@gamil.com>
2019-11-07 16:01:29 +0200

    Array

--- index.php
+++ index.php
@@ -1,45 +1,40 @@
 <?php
 
 $array = [
-    ['lentyna' => ['viršutinė' =>
-            [
-                ['produktas' => 'Kiaušiniai',
-                    'kiekis' => 3,
-                ],
-                [
-                    'produktas' => 'Grietinė',
-                    'kiekis' => 1,
-                ],
-                [
-                    'produktas' => 'Jogurtas',
-                    'kiekis' => 2,
-                ],
-                [
-                    'produktas' => 'Žuvis',
-                    'kiekis' => 0,
-                ],
-            ],
+    ['viršutinė lentyna' =>
+        ['produktas' => 'Kiaušiniai',
+            'kiekis' => 3,
         ],
-        ['apatinė' =>
-            [
-                ['produktas' => 'Pienas',
-                    'kiekis' => 1,
-                ],
-                [
-                    'produktas' => 'Kefyras',
-                    'kiekis' => 0,
-                ],
-                [
-                    'produktas' => 'Degtinė',
-                    'kiekis' => 2,
-                ],
-                [
-                    'produktas' => 'Kečupas',
-                    'kiekis' => 2,
-                ],
-            ],
+        [
+            'produktas' => 'Grietinė',
+            'kiekis' => 1,
         ],
-    ]
+        [
+            'produktas' => 'Jogurtas',
+            'kiekis' => 2,
+        ],
+        [
+            'produktas' => 'Žuvis',
+            'kiekis' => 0,
+        ],
+    ],
+    ['apatinė lentyna' =>
+        ['produktas' => 'Pienas',
+            'kiekis' => 1,
+        ],
+        [
+            'produktas' => 'Kefyras',
+            'kiekis' => 0,
+        ],
+        [
+            'produktas' => 'Degtinė',
+            'kiekis' => 2,
+        ],
+        [
+            'produktas' => 'Kečupas',
+            'kiekis' => 2,
+        ],
+    ],
 ];
 
 var_dump($array);
